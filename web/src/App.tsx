195d8c2d382d0ad1b850a327b96e3e47db94de7b
Benjamin Cooper <bencooper222@gmail.com>
2018-06-23 02:12:45 -0700

    Switched to the harder way

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
-    return (
+          return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
